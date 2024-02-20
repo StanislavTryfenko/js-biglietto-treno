42a94bdeb8e109c280c99ad3704a06e325e53d8a
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-20 06:13:38 +0100

    Improvement on code's readability

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,18 @@ const km = Number(prompt("Quanti km devi percorrere?"));
 
 const age = Number(prompt("Quanti anni hai?"));
 
-if( age < 18){
-    discountJunion = ((100-20)%);
+const discountJunion = ((100-20) / 100);
+
+const discountSenion = ((100-40) / 100);
+
+if (age < 18){  
     price = (Number(km) * 0.21 * Number(discountJunion));
 }
 else if (age > 65) {
-    discountSenion = ((100-40)%);
     price = (Number(km) * 0.21 * Number(discountSenion));
 } 
 else {
-    price = (Number(km) * 0.21);
+    price = "(Number(km) * 0.21)";
 }
 
 alert("Il biglietto costa: \n \n" + price.toFixed(2));
